b94c1012ee7cc0f702a73f50fdff7d10a207663c
pokoot <pokoot@gmail.com>
2014-10-16 23:35:25 +0800

    Init

---
+++ forums/index.php
@@ -0,0 +1,8 @@
+<?php
+header("Location:http://www.geocities.com/partypoooperz/includes/error/construction.html");
+/* Redirect browser */
+
+/* Make sure that code below does not get executed when we redirect. */
+exit;
+?> 
+
